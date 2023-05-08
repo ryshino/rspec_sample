9afa897847f72694c60ab28bf2ee1d0f956d465c
ryshino <ranature1153@gmail.com>
2023-05-09 07:12:35 +0900

    bookモデルを使ってテスト

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
-
 RSpec.describe Book, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "trueであるとき、falseになること" do # itの後にNG時に表示される "説明文" を書く
+    expect(true).to eq(true)
+    # expect(テスト対象コード).to マッチャー(想定テスト結果)
+    # マッチャーとはマッチ(一致)するかを判定する道具です
+    # マッチャーはここでは==で一致判定するeqをつかっています
+  end
+
+  it "Bookモデルをnewしたとき、nilではないこと" do
+    expect(Book.new).not_to eq(nil)
+  end
 end

3a697a087989704f05dc0dada3a80825e69b1a56
ryshino <ranature1153@gmail.com>
2023-05-10 07:11:05 +0900

    title_with_authorメソッドをテスト

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -10,4 +10,11 @@ RSpec.describe Book, type: :model do
   it "Bookモデルをnewしたとき、nilではないこと" do
     expect(Book.new).not_to eq(nil)
   end
+
+  describe "Book#title_with_author" do # describeメソッドをつかってメソッドごとに区切ると読みやすいです
+    it "Book#title_with_authorを呼び出したとき、titleとauthorを結んだ文字列が返ること" do
+      book = Book.new(title: "RubyBook", author: "matz")
+      expect(book.title_with_author).to eq("RubyBook - matz")
+    end
+  end
 end
